c78a63539077c5e5c2764d4ad2884a82be191cbc
BayuDC <dev.bayudc@gmail.com>
2022-05-26 06:27:47 +0700

    Handle user login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,7 @@
+const bcrypt = require('bcrypt');
+const createError = require('http-errors');
 const db = require('../db');
+
 /**
  * @param {import('express').Request} req
  * @param {import('express').Response} res
@@ -10,10 +13,26 @@ module.exports.me = (req, res, next) => {};
  * @param {import('express').Response} res
  * @param {import('express').NextFunction} next
  */
-module.exports.login = (req, res, next) => {};
+module.exports.login = async (req, res, next) => {
+    const { username, password } = req.body;
+
+    const user = await db.user.findUnique({
+        where: { username },
+    });
+    if (!user) return next(createError(404, 'User not found'));
+
+    const auth = await bcrypt.compare(password, user.password);
+    if (!auth) return next(createError(401, 'Incorrect password'));
+
+    res.json({
+        message: 'Login success',
+    });
+};
 /**
  * @param {import('express').Request} req
  * @param {import('express').Response} res
  * @param {import('express').NextFunction} next
  */
-module.exports.logout = (req, res, next) => {};
+module.exports.logout = (req, res, next) => {
+    // ! coming soon with redis
+};
