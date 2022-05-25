8fc3197d44a766df0f9a6d3a5aac9b2e72b88e7d
BayuDC <dev.bayudc@gmail.com>
2022-05-26 04:57:04 +0700

    Setup prisma

---
+++ server/prisma/seed.js
@@ -0,0 +1,19 @@
+const { faker } = require('@faker-js/faker');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+async function main() {
+    for (let i = 0; i < 10; i++) {
+        await prisma.user.create({
+            data: {
+                username: faker.internet.userName(),
+                password: '$2b$10$Hm3IPt5xdLKKr/8HfUHWxuemOpDZyikaDBtScq.1GsCTSEGqGTXry', // ? password
+            },
+        });
+    }
+}
+
+main()
+    .catch(e => console.log(e))
+    .finally(() => prisma.$disconnect());
