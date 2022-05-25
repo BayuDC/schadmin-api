0a067e9bc6525b677ab1d2a26dda267bf9de69b4
BayuDC <dev.bayudc@gmail.com>
2022-05-26 05:07:35 +0700

    Define auth routes and controller

---
+++ server/controllers/auth.js
@@ -0,0 +1,18 @@
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+module.exports.me = (req, res, next) => {};
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+module.exports.login = (req, res, next) => {};
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+module.exports.logout = (req, res, next) => {};
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
+const authController = require('../controllers/auth');
 
 router.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World!'
-    });
+    res.json({ message: 'Hello World!' });
 });
+
+router.get('/auth/me', authController.me);
+router.get('/auth/login', authController.login);
+router.get('/auth/logout', authController.logout);
 
 module.exports = router;
