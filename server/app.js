94a99746abbaa6da2e344608c8eaed98a5a71de5
BayuDC <dev.bayudc@gmail.com>
2022-05-25 14:17:46 +0700

    Setup express

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cookie = require('cookie-parser');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(cookie());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', require('./routes/web'));
+app.use('/api', require('./routes/api'));
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
