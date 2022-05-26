1460bc429368a86683d17a98ceb5d50dcbf4ccee
BayuDC <dev.bayudc@gmail.com>
2022-05-26 08:14:58 +0700

    Error handing

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -6,17 +6,10 @@ const db = require('../db');
 const secret = process.env.JWT_SECRET || 'a';
 
 const createToken = user => {
-    const payload = {
-        user: {
-            id: user.id,
-            username: user.username,
-        },
-    };
+    const payload = { user };
     const options = { expiresIn: 24 * 60 * 60 };
 
-    const token = jwt.sign(payload, secret, options);
-
-    return token;
+    return jwt.sign(payload, secret, options);
 };
 
 /**
@@ -34,17 +27,20 @@ module.exports.login = async (req, res, next) => {
     const { username, password } = req.body;
 
     const user = await db.user.findUnique({
-        where: { username },
+        where: { username: username || '' },
     });
     if (!user) return next(createError(404, 'User not found'));
 
-    const auth = await bcrypt.compare(password, user.password);
+    const auth = await bcrypt.compare(password || '', user.password);
     if (!auth) return next(createError(401, 'Incorrect password'));
+
+    delete user.password;
 
     const token = createToken(user);
 
     res.json({
         message: 'Login success',
+        user,
         token,
     });
 };
@@ -53,6 +49,4 @@ module.exports.login = async (req, res, next) => {
  * @param {import('express').Response} res
  * @param {import('express').NextFunction} next
  */
-module.exports.logout = (req, res, next) => {
-    // ! coming soon with redis
-};
+module.exports.logout = (req, res, next) => {};
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,12 +1,15 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth');
+const errorMiddleware = require('../middlewares/error');
 
 router.get('/', (req, res) => {
     res.json({ message: 'Hello World!' });
 });
 
 router.get('/auth/me', authController.me);
-router.get('/auth/login', authController.login);
+router.post('/auth/login', authController.login);
 router.get('/auth/logout', authController.logout);
+
+router.use(errorMiddleware.handle);
 
 module.exports = router;
