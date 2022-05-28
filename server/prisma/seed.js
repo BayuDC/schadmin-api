e9f4d4d9cb4af0c0c3b5fcd7c2497dc33ecdf766
BayuDC <dev.bayudc@gmail.com>
2022-05-28 09:30:05 +0700

    Update database seeder

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -13,8 +13,28 @@ async function main() {
         },
     });
 
-    await prisma.subject.create({ data: { name: 'Mathematic' } });
-    await prisma.subject.create({ data: { name: 'English' } });
+    for (subject of ['Mathematic', 'English']) {
+        await prisma.subject.create({
+            data: {
+                name: subject,
+                teachers: {
+                    create: [...Array(5)].map(() => ({
+                        name: faker.name.findName(),
+                        code: faker.random.alpha({ count: 2, casing: 'upper' }),
+                        gender: faker.helpers.arrayElement(['male', 'female']),
+                        address: faker.address.streetAddress(),
+                        user: {
+                            create: {
+                                username: faker.internet.userName(),
+                                password: password,
+                                role: 'teacher',
+                            },
+                        },
+                    })),
+                },
+            },
+        });
+    }
 }
 
 main()
